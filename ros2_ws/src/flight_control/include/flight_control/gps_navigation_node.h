1def122ff226004a2dd7dab94f57ed046720efcc
xuguocai <x3519610554@outlook.com>
2025-06-17 11:32:58 +0800

    ✨ feat: 增加common_msgs + common_msgs增加TargetGps自定义消息 + gps_navigation_node使用自定义消息

--- ros2_ws/src/flight_control/include/flight_control/gps_navigation_node.h
+++ ros2_ws/src/flight_control/include/flight_control/gps_navigation_node.h
@@ -4,14 +4,17 @@
 #include <rclcpp/rclcpp.hpp>
 #include <px4_msgs/msg/trajectory_setpoint.hpp>
 #include <px4_msgs/msg/vehicle_global_position.hpp>
+#include <common_msgs/msg/target_gps.hpp>
 
 class GpsNavigationNode : public rclcpp::Node {
 public:
     GpsNavigationNode();
 protected:
     void global_position_callback(const px4_msgs::msg::VehicleGlobalPosition::SharedPtr msg);
+    void target_gps_callback(const common_msgs::msg::TargetGps::SharedPtr msg);
 private:
     rclcpp::Subscription<px4_msgs::msg::VehicleGlobalPosition>::SharedPtr m_global_position_subscription;
+    rclcpp::Subscription<common_msgs::msg::TargetGps>::SharedPtr m_target_gps;
 };
 
 #endif
--- ros2_ws/src/flight_control/src/gps_navigation_node.cpp
+++ ros2_ws/src/flight_control/src/gps_navigation_node.cpp
@@ -1,21 +1,32 @@
 #include "flight_control/gps_navigation_node.h"
-#include <rclcpp/node.hpp>
+#include <common_msgs/msg/detail/target_gps__struct.hpp>
+#include <functional>
 
 using std::placeholders::_1;
 
 GpsNavigationNode::GpsNavigationNode() : rclcpp::Node("gps_navigation_node") {
     rclcpp::QoS qos(rclcpp::KeepLast(10));
     qos.best_effort();
+    m_target_gps = create_subscription<common_msgs::msg::TargetGps>(
+        "/target_gps", 10, 
+        std::bind(&GpsNavigationNode::target_gps_callback, this, _1));
     m_global_position_subscription = this->create_subscription<px4_msgs::msg::VehicleGlobalPosition>(
-            "/fmu/out/vehicle_global_position", qos,
-            std::bind(&GpsNavigationNode::global_position_callback, this, _1));
+        "/fmu/out/vehicle_global_position", qos,
+        std::bind(&GpsNavigationNode::global_position_callback, this, _1));
 }
 
 void GpsNavigationNode::global_position_callback(const px4_msgs::msg::VehicleGlobalPosition::SharedPtr msg){
 
-    double latitude = msg->lat;
-    double longitude = msg->lon;
-    RCLCPP_INFO(this->get_logger(), "Latitude: %.7f, Longitude: %.7f", latitude, longitude);    
+    // double latitude = msg->lat;
+    // double longitude = msg->lon;
+    // RCLCPP_INFO(this->get_logger(), "Latitude: %.7f, Longitude: %.7f", latitude, longitude);    
+}
+
+void GpsNavigationNode::target_gps_callback(const common_msgs::msg::TargetGps::SharedPtr msg){
+
+    double latitude = msg->latitude;
+    double longitude = msg->longitude;
+    RCLCPP_INFO(this->get_logger(), "Target_latitude: %.7f, Target_Longitude: %.7f", latitude, longitude);    
 }
 
 int main(int argc, char *argv[]) {
