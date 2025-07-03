e3edb8aa02723b7bb5ea57b07b2d6f79d604be30
xuguocai <x3519610554@outlook.com>
2025-07-03 17:53:54 +0800

    🎈 perf: 仿真启动文件正常加载世界

--- ros2_ws/src/simulation/launch/simulation.launch.py
+++ ros2_ws/src/simulation/launch/simulation.launch.py
@@ -1,10 +1,11 @@
+import os
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable
+from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.actions import IncludeLaunchDescription
 from ament_index_python.packages import get_package_share_directory
-import os
+from launch.actions import SetEnvironmentVariable
 
 def generate_launch_description():
     pkg_sim = get_package_share_directory('simulation')
@@ -14,19 +15,16 @@ def generate_launch_description():
     world_path = os.path.join(pkg_sim, 'worlds', 'landing_place.world')
     gazebo_launch_path = os.path.join(pkg_gz, 'launch', 'gazebo.launch.py')
 
-    # 设置 GAZEBO_MODEL_PATH 环境变量
     set_gazebo_model_path = SetEnvironmentVariable(
         name='GAZEBO_MODEL_PATH',
         value=model_path
     )
 
-    # 设置 world 参数
     world_arg = DeclareLaunchArgument(
         'world',
         default_value=world_path,
     )
 
-    # 加载 gazebo_ros 的启动文件
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(gazebo_launch_path),
         launch_arguments={'world': LaunchConfiguration('world')}.items()
@@ -37,3 +35,4 @@ def generate_launch_description():
         world_arg,
         gazebo
     ])
+
