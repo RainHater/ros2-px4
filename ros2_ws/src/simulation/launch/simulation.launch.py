83fe3816142e01728afddaecb6fed19b228a99cb
xuguocai <x3519610554@outlook.com>
2025-07-04 17:02:32 +0800

    🎈 perf: 增加无人机模型启动

--- ros2_ws/src/simulation/launch/simulation.launch.py
+++ ros2_ws/src/simulation/launch/simulation.launch.py
@@ -6,6 +6,8 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.actions import IncludeLaunchDescription
 from ament_index_python.packages import get_package_share_directory
 from launch.actions import SetEnvironmentVariable
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
 
 def generate_launch_description():
     pkg_sim = get_package_share_directory('simulation')
@@ -13,6 +15,7 @@ def generate_launch_description():
 
     model_path = os.path.join(pkg_sim, 'models')
     world_path = os.path.join(pkg_sim, 'worlds', 'landing_place.world')
+    model_sdf_path = os.path.join(pkg_sim, 'models', 'iris_downward_depth_camera', 'iris_downward_depth_camera.sdf')
     gazebo_launch_path = os.path.join(pkg_gz, 'launch', 'gazebo.launch.py')
 
     set_gazebo_model_path = SetEnvironmentVariable(
@@ -30,9 +33,19 @@ def generate_launch_description():
         launch_arguments={'world': LaunchConfiguration('world')}.items()
     )
 
+    spawn_model = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-entity', 'iris_drone_1',
+                   '-file', model_sdf_path,
+                   '-x', '1.0', '-y', '2.0', '-z', '0.5'],
+        output='screen'
+    )
+
     return LaunchDescription([
         set_gazebo_model_path,
         world_arg,
-        gazebo
+        gazebo,
+        spawn_model,
     ])
 
