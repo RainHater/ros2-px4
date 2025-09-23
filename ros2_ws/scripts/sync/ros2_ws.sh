6d948225f8da105dfa36882bade760559f2d4893
xuguocai <x3519610554@outlook.com>
2025-09-23 15:25:21 +0800

    🎈 perf: 修改了脚本

--- ros2_ws/scripts/sync/ros2_ws.sh
+++ ros2_ws/scripts/sync/ros2_ws.sh
@@ -4,34 +4,40 @@
 
 TOP_DIR=$(pwd)
 
-TARGET_IP="100.76.77.51"
-TARGET_USER="sunrise"
-TARGET_PATH="/home/${TARGET_USER}/ros2_px4/ros2_ws"
-SOURCE_PATH="${TOP_DIR}/install/"
+echo "当前目录: ${TOP_DIR}"
 
-echo "开始同步到 $TARGET_USER@$TARGET_IP:$TARGET_PATH"
+TARGET_IP="100.76.77.51"
+# TARGET_IP="172.20.10.3"
+# TARGET_IP="192.168.0.204"
+TARGET_USER="sunrise"
+TARGET_PASSWORD="sunrise"
+TARGET_PATH="/home/${TARGET_USER}/ros2_px4/ros2_ws"
+SOURCE_PATH="${TOP_DIR}/arm/install/"
+
+echo "源文件目录: ${SOURCE_PATH}"
+echo "开始同步到 ${TARGET_USER}@${TARGET_IP}:${TARGET_PATH}"
 
 # 本地路径检测
-if [ ! -d "$SOURCE_PATH" ]; then
-    echo "❌ 本地目录不存在: $SOURCE_PATH"
+if [ ! -d "${SOURCE_PATH}" ]; then
+    echo "❌ 本地目录不存在: ${SOURCE_PATH}"
     exit 1
 fi
 
 # 远端目录检测（通过 ssh 远程执行 mkdir -p，保证目录存在）
-sshpass -p "sunrise" ssh $TARGET_USER@$TARGET_IP "mkdir -p $TARGET_PATH/install"
+sshpass -p "${TARGET_PASSWORD}" ssh ${TARGET_USER}@${TARGET_IP} "mkdir -p ${TARGET_PATH}/install"
 
 # 同步
-sshpass -p "sunrise" rsync -aAXv \
-    "$SOURCE_PATH" \
-    "$TARGET_USER@$TARGET_IP:$TARGET_PATH/install"
-
-sshpass -p "sunrise" rsync -aAXv \
-    "$SOURCE_PATH/utilities/share/utilities/config/app.yaml" \
-    "./yaml_update.sh" \
-    "$TARGET_USER@$TARGET_IP:$TARGET_PATH"
+sshpass -p "${TARGET_PASSWORD}" rsync -aAXv \
+    "${SOURCE_PATH}" \
+    "${TARGET_USER}@${TARGET_IP}:${TARGET_PATH}/install"
 
 sshpass -p "${TARGET_PASSWORD}" rsync -aAXv \
-    "./track_start.sh" \
-    "${TARGET_USER}@${TARGET_IP}:/bin/"
+    "${SOURCE_PATH}/utilities/share/utilities/config/app.yaml" \
+    "${SOURCE_PATH}/mipi_detect/share/mipi_detect/config/m_params.yaml" \
+    "${SOURCE_PATH}/mipi_detect/share/mipi_detect/config/params.yaml" \
+    "${SOURCE_PATH}/mipi_detect/share/mipi_detect/config/config.json" \
+    "${TOP_DIR}/scripts/target/yaml_update.sh" \
+    "${TOP_DIR}/scripts/target/track_start.sh" \
+    "${TARGET_USER}@${TARGET_IP}:${TARGET_PATH}"
 
 echo "✅ 同步完成"
